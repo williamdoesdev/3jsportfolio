3c59e58569f82afb3c07870a5ac89da163ca193f
William Simpson <william@williamdoes.dev>
2022-05-01 11:16:01 -0500

    Adjusted lighting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import { createRoot } from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense } from "react";
+import { useMemo } from "react";
 
 import Background from "./components/background/background";
-import Room from "./components/three/room";
+
+import Model from "./components/three/Room.js";
 
 function App() {
   return (
     <div className="container">
       <Background />
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Room />
-      </Canvas>
+      <div className="canvas-container">
+        <Canvas shadows>
+          <Suspense fallback={null}>
+            <ambientLight intensity={0.125} color={"#B9DDFF"} />
+            <directionalLight
+              position={[10, 7.5, 20]}
+              lookAt={[0, 0, 0]}
+              castShadow={true}
+              shadow-mapSize-height={512}
+              shadow-mapSize-width={512}
+              intensity={4}
+              color={"#F8C585"}
+            />
+            <pointLight
+              position={[0.5, 2, -0.5]}
+              distance={3}
+              decay={1}
+              intensity={1.5}
+              color={"#D1EBFF"}
+            />
+            <Model scale={0.5} />
+            <OrbitControls zoom={1} />
+          </Suspense>
+        </Canvas>
+      </div>
     </div>
   );
 }
