24b28fc0eaefc9ac87d5184e6aea15dc06fc37e1
William <william@williamdoes.dev>
2022-05-25 16:52:27 -0500

    fixed issue with start screen and replaced favicon

--- src/components/startScreen.js
+++ src/components/startScreen.js
@@ -17,52 +17,60 @@ import "../scss/startScreen.scss";
 export default function StartScreen(props) {
   return (
     <div className="start-screen">
-      <div className="start-title-container">
-        <h1>William Does.</h1>
-        <div className="start-title-cursor"></div>
-      </div>
-      <div className="start-icons-grid">
-        <img src={HTMLIcon} style={{ gridRowStart: 1, gridColumnStart: 1 }} />
-        <img src={CSSIcon} style={{ gridRowStart: 1, gridColumnStart: 2 }} />
-        <img src={JSIcon} style={{ gridRowStart: 1, gridColumnStart: 3 }} />
-        <img src={ReactIcon} style={{ gridRowStart: 2, gridColumnStart: 1 }} />
-        <img src={NodeIcon} style={{ gridRowStart: 2, gridColumnStart: 2 }} />
-        <img src={MongoIcon} style={{ gridRowStart: 2, gridColumnStart: 3 }} />
-        <img
-          src={IllIcon}
-          designIcon="true"
-          style={{ gridRowStart: 1, gridColumnStart: 1 }}
-        />
-        <img
-          src={IndIcon}
-          designIcon="true"
-          style={{ gridRowStart: 1, gridColumnStart: 2 }}
-        />
-        <img
-          src={AEIcon}
-          designIcon="true"
-          style={{ gridRowStart: 1, gridColumnStart: 3 }}
-        />
-        <img
-          src={PSIcon}
-          designIcon="true"
-          style={{ gridRowStart: 2, gridColumnStart: 1 }}
-        />
-        <img
-          src={LRIcon}
-          designIcon="true"
-          style={{ gridRowStart: 2, gridColumnStart: 2 }}
-        />
-      </div>
-      <div className="start-link">
-        <a
-          onClick={() => {
-            props.onToggleOrbitControls();
-            props.onToggleStartScreen(0);
-          }}
-        >
-          Click Here to Meet Me.
-        </a>
+      <div className="start-screen-content">
+        <div className="start-title-container">
+          <h1>William Does.</h1>
+          <div className="start-title-cursor"></div>
+        </div>
+        <div className="start-icons-grid">
+          <img src={HTMLIcon} style={{ gridRowStart: 1, gridColumnStart: 1 }} />
+          <img src={CSSIcon} style={{ gridRowStart: 1, gridColumnStart: 2 }} />
+          <img src={JSIcon} style={{ gridRowStart: 1, gridColumnStart: 3 }} />
+          <img
+            src={ReactIcon}
+            style={{ gridRowStart: 2, gridColumnStart: 1 }}
+          />
+          <img src={NodeIcon} style={{ gridRowStart: 2, gridColumnStart: 2 }} />
+          <img
+            src={MongoIcon}
+            style={{ gridRowStart: 2, gridColumnStart: 3 }}
+          />
+          <img
+            src={IllIcon}
+            designIcon="true"
+            style={{ gridRowStart: 1, gridColumnStart: 1 }}
+          />
+          <img
+            src={IndIcon}
+            designIcon="true"
+            style={{ gridRowStart: 1, gridColumnStart: 2 }}
+          />
+          <img
+            src={AEIcon}
+            designIcon="true"
+            style={{ gridRowStart: 1, gridColumnStart: 3 }}
+          />
+          <img
+            src={PSIcon}
+            designIcon="true"
+            style={{ gridRowStart: 2, gridColumnStart: 1 }}
+          />
+          <img
+            src={LRIcon}
+            designIcon="true"
+            style={{ gridRowStart: 2, gridColumnStart: 2 }}
+          />
+        </div>
+        <div className="start-link">
+          <a
+            onClick={() => {
+              props.onToggleOrbitControls();
+              props.onToggleStartScreen(0);
+            }}
+          >
+            Click Here to Meet Me.
+          </a>
+        </div>
       </div>
     </div>
   );
